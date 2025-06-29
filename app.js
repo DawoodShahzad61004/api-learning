8828cd0038c857e44519d741b248619d9267fab6
DawoodShahzad61004 <dawoodbutt590@gmail.com>
2025-06-30 04:29:19 +0500

    feat: add error handling middleware and logging with morgan

--- app.js
+++ app.js
@@ -1,11 +1,28 @@
 import express from 'express';
 import productRoutes from './api/routes/products.js';
 import orderRoutes from './api/routes/orders.js';
+import morgan from 'morgan';
 
 const app = express();
 const PORT = 3000;
+app.use(morgan('dev'));
 
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
+
+app.use((req, res, next) => {
+    const error = new Error('Not Found');
+    error.status = 404;
+    next(error);
+});
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    });
+});
 
 export default app;
